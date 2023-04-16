8aa200b54483474c9b1371ea7ba023b59a63f845
Steve Galea <steve.galea.20@um.edu.mt>
2023-04-16 20:34:26 +0200

    Form commit(omitted link)

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          <code>In Progress...</code>.
+          <code>In Progress...</code>
         </p>
         <a
           className="App-link"
