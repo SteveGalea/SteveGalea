55bca52d453c286c72aa617038f2bbea5112eee6
Steve Galea <steve.galea.20@um.edu.mt>
2023-04-25 14:17:12 +0200

    Update

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -47,7 +47,7 @@ function App() {
           <p className="text">
             Welcome to my portfolio! I am currently in my third and final year of my Bachelor's in Software Development at the University of Malta. I am always seeking to expand my skills and explore new areas of interest within the technological domain.</p><br/>
           <p className='text'>
-            When I am not coding or working on new projects, I keep myself focused and energized by practicing sports such as football, running, jogging, and cycling! In addition to my academic pursuits and hobbies, I am deeply committed to making a positive impact in my local community. Together, with my friends at Nadur Nadif, we promote positive change in our community through clean-ups and education campaigns.</p><br/>
+            When I am not coding or working on new projects, I keep myself focused and energized by practicing sports such as football, running, jogging, and cycling! In addition to my academic pursuits and hobbies, I am deeply committed to making a positive impact in my local community. Together, with my friends at <a href="https://www.facebook.com/nadurnadif">Nadur Nadif</a>, we promote positive change in our community through clean-ups and education campaigns.</p><br/>
           <p className='text'>
             Looking ahead, I plan to continue my education by pursuing a master's degree next year. I am excited to see where my skills will take me, and I look forward to sharing my favourite projects on this portfolio website!</p><br/>
           <p className='text text-center'>
